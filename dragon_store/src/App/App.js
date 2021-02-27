e85cbdaa2c3b5754a12851caca43693e5b0b1713
Edson <edsont8@hotmail.com>
2021-02-27 19:11:46 -0300

    Side menu style

---
+++ dragon_store/src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SideMenu from '../components/SideMenu';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <SideMenu />
+      <div>here we go</div>
+    </React.Fragment>
+
+  );
+}
+
+export default App;
