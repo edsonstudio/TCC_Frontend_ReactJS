15e287d0fa0af52f02a2137822dfd2db827edc6e
Edson <edsont8@hotmail.com>
2021-02-28 12:37:47 -0300

    finish style header

--- dragon_store/src/App/App.js
+++ dragon_store/src/App/App.js
@@ -1,26 +1,44 @@
-import { CssBaseline, makeStyles } from '@material-ui/core';
 import React from 'react';
+import './App.css';
 import Header from '../components/Header';
 import SideMenu from '../components/SideMenu';
-import './App.css';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
+
+// Customization style from https://material-ui.com/customization/default-theme/
+
+const theme = createMuiTheme({
+  palette:{
+    primary:{
+      main: "#333996",
+      light: '#3C44B126'
+    },
+    secondary:{
+      main: "#F83245",
+      light: '#F8324526'
+    },
+    background:{
+      default:"#F4F5FD"
+    }
+  }
+})
 
 const useStyles = makeStyles({
   appMain: {
     paddingLeft:'320px',
     width:'100%'
   }
-});
+})
 
 function App() {
   const classes = useStyles();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
-      <Header />
+        <Header />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 
